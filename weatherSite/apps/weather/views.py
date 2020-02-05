277e113952ce17392348aa341ef57295f890297e
Akezh <rakishev.akezhan@mail.ru>
2020-02-05 16:59:26 +0600

    fix: additional changes

--- weatherSite/apps/weather/views.py
+++ weatherSite/apps/weather/views.py
@@ -1,19 +1,28 @@
 # from django.http import HttpResponse
 import requests
 from django.shortcuts import render
+from .models import City
 
 def index(request):
   url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=535ba2a3c695c636ae722cc1f7ff3ed3'
-  city = 'Las Vegas'
+  city = 'Astana'
 
-  r = requests.get(url.format(city)).json()
+  cities = City.objects.all()
 
-  city_weather = {
-    'city': city,
-    'temperature': r['main']['temp'],
-    'description': r['weather'][0]['description'],
-    'icon': r['weather'][0]['icon']
-  }
+  weather_data = []
+
+  for city in cities:
+    r = requests.get(url.format(city)).json()
+
+    city_weather = {
+      'city': city,
+      'temperature': r['main']['temp'],
+      'description': r['weather'][0]['description'],
+      'icon': r['weather'][0]['icon']
+    }
+
+    weather_data.append(city_weather)
+  print (weather_data)
 
   context = {'city_weather' : city_weather}
   
--- weatherSite/settings.py
+++ weatherSite/settings.py
@@ -40,8 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'weatherSite.apps.weather'
+    'django.contrib.sites',
+    'weatherSite.apps.weather',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
