4500a6f79eb46e58cdd45aa704a6ad521acf5f31
Akezh <rakishev.akezhan@mail.ru>
2020-02-05 17:02:03 +0600

    refactor: project deletion for restructuring

--- weatherSite/apps/weather/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import City
-
-admin.site.register(City)
--- weatherSite/apps/weather/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WeatherConfig(AppConfig):
-    name = 'weather'
